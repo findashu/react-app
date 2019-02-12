f9a17eb3b92d1a4d9d2be7ed947c18e6a68d01b7
Ashu_Mishra <ashutosh@thruskills.com>
2019-02-12 14:28:25 +0530

    multiple components and combining them in one component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import MainComponent from "./components/MainComponent";
+import Footer from "./components/Footer";
+
+function App () {
+    return (
+        <div>
+            <Header />
+            <MainComponent />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyInfo from "./MyInfo"
+function MainComponent() {
+    return (
+        <main>
+            <MyInfo />
+        </main>
+    )
+}
+
+export default MainComponent
