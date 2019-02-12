2c5e625cf2f5302d69ce54ceffd117f8ae407956
Ashu_Mishra <ashutosh@thruskills.com>
2019-02-12 13:46:12 +0530

    basic structure

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Hello! My Name Ashutosh Mishra</h1>
+            <p>I am Software Engineer, I read write and code alot</p>
+            <ul>
+                <li>Mysore</li>
+                <li>Kerala</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
