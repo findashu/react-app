48e4869f48960a7f608c1692c160af5bd9484d63
Ashu_Mishra <ashutosh@thruskills.com>
2019-02-12 14:34:17 +0530

    todo app phase1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Header from "./components/Header";
-import MainComponent from "./components/MainComponent";
-import Footer from "./components/Footer";
 
 function App () {
     return (
         <div>
-            <Header />
-            <MainComponent />
-            <Footer />
+            <input type="checkbox" />
+            <p>Placeholder Text</p>
+            <input type="checkbox" />
+            <p>Placeholder Text</p>
+            <input type="checkbox" />
+            <p>Placeholder Text</p>
+            <input type="checkbox" />
+            <p>Placeholder Text</p>
         </div>
     )
 }
