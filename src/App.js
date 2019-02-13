ec63d90ed5506fe3fa3096b7bc43e348440807e0
Ashu_Mishra <ashutosh@thruskills.com>
2019-02-13 14:01:20 +0530

    class based components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React from "react";
-import TodoItem from "./components/TodoItem"
-import todosData from "./todosData";
+import React, {Component} from "react";
 
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header username="ashu" />
+                <Greetings />
+            </div>
+        )
+    }
+}
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h2>Welcome, {this.props.username}</h2>
+            </header>
+        )
+    }
+}
 
+class Greetings extends Component {
+    render () {
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
 
-function App () {
-    let todosComponent = todosData.map(todo => <TodoItem todo={todo} key={todo.id} />)
-
-    return (
-        <div className="todo-list">
-            {todosComponent}
-        </div>
-    )
+        if (hours < 12) {
+            timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17) {
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+        return (
+            <h1>Good {timeOfDay} to you, sir or madam!</h1>
+        )
+    }
 }
 
 export default App
