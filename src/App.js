fa9351199ef427a877e60ced56dee206b5dae8a7
Ashu_Mishra <ashutosh@thruskills.com>
2019-03-10 19:56:09 +0530

    fixed state

--- src/App.js
+++ src/App.js
@@ -10,9 +10,25 @@ class App extends React.Component {
         this.state = {
             todos:todosData
         }
+        this.handleOnChange = this.handleOnChange.bind(this)
     }
+
+    handleOnChange(id) {
+        this.setState(prevState => {
+           let updatedState =  prevState.todos.map(todo => {
+                if(todo.id === id){
+                    todo.completed =  !todo.completed ;
+                }
+                return todo;
+            })
+            return {todos:updatedState}
+        })
+    }
+
+
+
     render() {
-        let todosComponent = this.state.todos.map(todo => <TodoItem todo={todo} key={todo.id} />)
+        let todosComponent = this.state.todos.map(todo => <TodoItem todo={todo} key={todo.id} handleChange={this.handleOnChange} />)
 
         return (
             <div className="todo-list">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" onChange={() => console.log('Changed')} checked={props.todo.completed} />
+            <input type="checkbox" onChange={(event) => props.handleChange(props.todo.id)} checked={props.todo.completed} />
             <p>{props.todo.text}</p>
         </div>
     )
